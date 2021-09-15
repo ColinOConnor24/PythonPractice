de82f6b2be93002e6ff2b53406e89cade337dabe
ColinOConnor24 <818704@mystma.org>
2021-09-15 09:13:15 -0500

    5 Lists

--- main.py
+++ main.py
@@ -1,18 +1,59 @@
 def topFiveMovies():
     #print the first movie
     print("Iron Man")
-
     #print the second movie
     print("The Avengers")
-
     #print the third movie
     print("Avengers: Age of Ultron")
-
     #print the fourth movie
     print("Avengers: Infinity War")
-
     #print the fifth movie
     print("Avengers: End Game")
-
 print("These are my top five movies")
 topFiveMovies()
+print(" ")
+def topThreeFoods():
+    #print first food
+    print("Pizza")
+    #print second food
+    print("Tacos")
+    #print third food
+    print("Cheeseburgers")
+print("These are my top three foods")
+topThreeFoods()
+print(" ")
+def favoriteVideoGamesOnPC():
+    #print first game
+    print("Minecraft")
+    #print second game
+    print("Rocket League")
+    #print third game
+    print("Splitgate")
+print("These are my favorite PC games right now")
+favoriteVideoGamesOnPC()
+print(" ")
+def favoriteVideoGamesOnMobile():
+    #print first game
+    print("Clash of Clans")
+    #print second game
+    print("Clash Royale")
+    #print third game
+    print("Diep.io")
+print("These are my favorite mobile games")
+favoriteVideoGamesOnMobile()
+print(" ")
+def myPets():
+    #print first pet
+    print("Ace")
+    #print second pet
+    print("Dakota")
+    #print third pet
+    print("Zoomer")
+    #print fourth pet
+    print("Zapper")
+    #print fifth pet
+    print("Sky")
+    #print sixth pet
+    print("Tom")
+print("These are the names of my pets")
+myPets()
