7ab9d41e30cd24078feb9283b6c17adcde84d8dc
ColinOConnor24 <818704@mystma.org>
2021-09-22 09:48:39 -0500

    Guessing Game

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-def madlib(adj1, noun1, verb1, adj2, noun2, noun3, verb2, date, noun4, verb3) :
-    print("There was a " + adj1 + " " + noun1 + " that was " + verb1 + " to the " + adj2 + " " + noun2 + ". The " + noun1 + " was with " + noun3 + ", they were talking about " + verb2 + " and what they would do on " + date + ". The " + noun1 + " said that they should go to the " + noun4 + " and " + verb3 + ".")
-madlib("tall", "man", "walking", "blue", "store", "friend", "fishing", "Wednesday", "boat", "fish")
+import random
 
-def starwarsmadlib(adj1, noun1, adj2, noun2, adj3, adj4, noun3, adj5, adj6, noun4, adj7, noun5, noun6, adj8, noun7, verb1, adj9, verb2, noun8, noun9, adj10, verb3, adj11) :
-    print('Star Wars is a ' + adj1 + ' ' + noun1 + ' of ' + adj2 + ' versus evil in a ' + noun2 + ' far far away. There are ' + adj3 + ' battles between ' + adj4 + ' ' + noun3 + ' in ' + adj5 + ' space and ' + adj6 + ' duels with ' + noun4 + ' called ' + adj7 + ' sabers. ')
-    print(noun5 + 'called "droids" are helpers and ' + noun6 + ' to the heroes. A ' + adj8 + ' power called The ' + noun7 + ' ' + verb1 + 's people to do ' + adj9 + ' things, like ' + verb2 + ' ' + noun8 + '. The Jedi ' + noun9 + ' use The Force for the ' + adj10)
-    print(' side and the Sith ' + verb3 + ' it for the ' + adj11 + ' side.')
+bottomofrange = input("What do you want the bottom of the range to be? ")
+topofrange = input("What do you want the top of the range to be? ")
+
+random_number = random.randint(int(bottomofrange), int(topofrange))
+
+guess = input("Guess what the random number is. ")
+
+while int(guess) != random_number:
+    if int(guess) > random_number:
+        print("Too high!")
+    else:
+        print("Too low!")
+    guess = input("Guess what the random number is. ")
+
+if int(guess) == random_number:
+    print("You have guessed the number correctly!")
