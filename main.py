3a7679dc0c53d72f016951a7016d6151cb560181
ColinOConnor24 <818704@mystma.org>
2021-09-30 09:43:11 -0500

    count to 10, 10 times

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def function(x):
+def function(x,y):
     count = 1
-    while count <= x:
-        print(count)
-        count += 1
-function(100)
+    overallcount = 1
+    while overallcount <= y:
+        while count <= x:
+            print(count)
+            count += 1
+        overallcount += 1
+        count = 1
+    
+function(10,10)
