49abe38487cbe6da4e057d98535c8cce348372ed
ColinOConnor24 <818704@mystma.org>
2021-10-01 09:16:19 -0500

    Number Guessing Game

--- second.py
+++ second.py
@@ -6,13 +6,16 @@ topofrange = input("What do you want the top of the range to be? ")
 random_number = random.randint(int(bottomofrange), int(topofrange))
 
 guess = input("Guess what the random number is. ")
+guesses = 1
 
 while int(guess) != random_number:
     if int(guess) > random_number:
         print("Too high!")
+        guesses += 1
     else:
         print("Too low!")
+        guesses += 1
     guess = input("Guess what the random number is. ")
 
 if int(guess) == random_number:
-    print("You have guessed the number correctly!")
+    print("You have guessed the number correctly in " + str(guesses) + " tries!")
