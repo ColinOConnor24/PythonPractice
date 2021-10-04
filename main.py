bbac1c18f978a2288ab98d18aafd15516cc8da3b
ColinOConnor24 <818704@mystma.org>
2021-10-04 08:53:52 -0500

    Word Speller

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 word = input("What word would you like me to spell? ")
-letters  = len(word)
 x = len(word)
-len2 = len(word) - 2
-while letters > 0:
-    print(word[x - len2])
-    x += 1
-    letters -= 1
+y = len(word)
+while y > 0:
+    print(word[x - y])
+    y -= 1
