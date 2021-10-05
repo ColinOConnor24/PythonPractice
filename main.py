f9e411f96ae3d2b1573f010f4e321949ea57aef6
ColinOConnor24 <818704@mystma.org>
2021-10-05 09:43:03 -0500

    Rock Paper Scissors

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
-for i in range(0,10):
-    print(i)
+import random
+
+user_wins = 0
+computer_wins = 0
+
+options = ["rock", "paper", "scissors"]
+
+while True:
+    user_input = input('Type Rock/Paper/Scissors, or Q to quit: ').lower()
+    if user_input == "q":
+        break
+    if user_input not in options:
+        continue
+    random_number = random.randint(0, 2)
+    computer_pick = options[random_number]
+    print("The computer picked", computer_pick + ".")
+
+    if (user_input == "rock" and computer_pick == "scissors") or (user_input == "paper" and computer_pick == "rock") or (user_input == "scissors" and computer_pick == "paper"):
+        print("You won!")
+        user_wins += 1
+    elif user_input == computer_pick:
+        print("Tie!")
+    else:
+        print("You lost!")
+        computer_wins += 1
+
+print("You won", user_wins, "times.")
+print("The computer won", computer_wins, "times.")
+if user_wins > computer_wins:
+    print("You won the entire game!")
+elif user_wins == computer_wins:
+    print("You tied the game with the computer!")
+else:
+    print("You lost the entire game!")
